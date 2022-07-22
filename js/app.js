e7c0982119d5c4854114dc84fafe08e26f363cb6
satinder <satindersingh472@gmail.com>
2022-07-22 12:30:34 -0600

    added all items with cookies

--- js/app.js
+++ js/app.js
@@ -4,8 +4,64 @@ let carrots = {
     price: `$5 per pounds`,
     image_url:`/home/satinder/Desktop/study/innotech/Assignments/w11/assignment_w11b/images/carrot.jpg`
 }
+let cucumbers = {
+    name: `cucumbers`,
+    description: `Fresh cucumbers from the fields`,
+    price: `$4 per pounds`,
+    image_url:`/home/satinder/Desktop/study/innotech/Assignments/w11/assignment_w11b/images/cucumber.jpg`
+}
+let potatoes = {
+    name: `potatoes`,
+    description: `Fresh potatoes from the fields`,
+    price: `$7 per pounds`,
+    image_url:`/home/satinder/Desktop/study/innotech/Assignments/w11/assignment_w11b/images/potato.jpg`
+}
+let squashes = {
+    name: `squashes`,
+    description: `Fresh squash from the fields`,
+    price: `$3 per pounds`,
+    image_url:`/home/satinder/Desktop/study/innotech/Assignments/w11/assignment_w11b/images/squash.jpg`
+}
+let tomatoes = {
+    name: `tomatoes`,
+    description: `Fresh tomatoes from the fields`,
+    price: `$2 per pounds`,
+    image_url:`/home/satinder/Desktop/study/innotech/Assignments/w11/assignment_w11b/images/tomato.jpg`
+}
+
+// set_cookies_carrots function is to convert carrots object to json and set cookies carrot
 function set_cookies_carrot(details){
+    let carrots_cookies = JSON.stringify(carrots);
     Cookies.set(`carrots`,carrots_cookies);
 }
-let carrots_cookies = JSON.stringify(carrots);
-carrots_cookies.addEventListener(`click`,set_cookies_carrot);
+
+function set_cookies_cucumber(details){
+    let cucumbers_cookies = JSON.stringify(cucumbers);
+    Cookies.set(`cucumbers`,cucumbers_cookies);
+}
+function set_cookies_potatoes(details){
+    let potatoes_cookies = JSON.stringify(potatoes);
+    Cookies.set(`potatoes`,potatoes_cookies);
+}
+function set_cookies_squash(details){
+    let squashes_cookies = JSON.stringify(squashes);
+    Cookies.set(`squashes`,squashes_cookies);
+}
+function set_cookies_tomatoes(details){
+    let tomatoes_cookies = JSON.stringify(tomatoes);
+    Cookies.set(`tomatoes`,tomatoes_cookies);
+}
+let carrot_order = document.getElementById(`buy_carrots`);
+carrot_order.addEventListener(`click`,set_cookies_carrot);
+
+let cucumber_order = document.getElementById(`buy_cucumbers`);
+cucumber_order.addEventListener(`click`,set_cookies_cucumber);
+
+let potato_order = document.getElementById(`buy_potatoes`);
+potato_order.addEventListener(`click`,set_cookies_potatoes);
+
+let squash_order = document.getElementById(`buy_squashes`);
+squash_order.addEventListener(`click`,set_cookies_squash);
+
+let tomato_order = document.getElementById(`buy_tomatoes`);
+tomato_order.addEventListener(`click`,set_cookies_tomatoes);
--- js/checkout.js
+++ js/checkout.js
@@ -1,14 +1,76 @@
-
-let cookies_carrots_json = Cookies.get(`carrots`);
-let cookies_carrots = JSON.parse(cookies_carrots_json);
+let main = document.getElementById(`main`);
+// function carrots display will display description,name,price and image of the cookie selected
 function carrots_display(details) {
-    document.getElementById(`main`)[`innerHTML`] =
+    main[`innerHTML`] +=
         `<section>
 <h3> You have Selected ${cookies_carrots[`name`]} </h3>
-<img src="${cookies_carrots[`image_url`]}" alt="carrots"/>
 <p> Price ${cookies_carrots[`price`]}</p>
 </section>`;
 }
+// cucumbers display function will display description of the item selected
+
+function cucumbers_display(details){
+    main[`innerHTML`] += 
+    `<section>
+    <h3>You have selected ${cookies_cucumber[`name`]}</h3>
+    <p>Price ${cookies_cucumber[`price`]}</p>
+    </section>`;
+}
+function potatoes_display(details){
+    main[`innerHTML`] += 
+    `<section>
+    <h3>You have selected ${cookies_potatoes[`name`]}</h3>
+    <p>Price ${cookies_potatoes[`price`]}</p>
+    </section>`;
+}
+function squashes_display(details){
+    main[`innerHTML`] += 
+    `<section>
+    <h3>You have selected ${cookies_squashes[`name`]}</h3>
+    <p>Price ${cookies_squashes[`price`]}</p>
+    </section>`;
+}
+function tomatoes_display(details){
+    main[`innerHTML`] += 
+    `<section>
+    <h3>You have selected ${cookies_tomatoes[`name`]}</h3>
+    <p>Price ${cookies_tomatoes[`price`]}</p>
+    </section>`;
+}
+
+// get the cookies related to carrot object 
+let cookies_carrots_json = Cookies.get(`carrots`);
+// convert cookies from json to normal string by doing parse
+let cookies_carrots = JSON.parse(cookies_carrots_json);
+
+// get the cookies related to cucumber
+let cookies_cucumbers_json = Cookies.get(`cucumbers`);
+// convert cookies from json to normal string
+let cookies_cucumber = JSON.parse(cookies_cucumbers_json);
+
+let cookies_potatoes_json = Cookies.get(`potatoes`);
+let cookies_potatoes = JSON.parse(cookies_potatoes_json);
+
+let cookies_squashes_json = Cookies.get(`squashes`);
+let cookies_squashes = JSON.parse(cookies_squashes_json);
+
+let cookies_tomatoes_json = Cookies.get(`tomatoes`);
+cookies_tomatoes = JSON.parse(cookies_tomatoes_json);
+
+
+//if statement will check if carrots cookies selected and has name value equal to carrots
 if (cookies_carrots[`name`] === `carrots`) {
     carrots_display();
+} 
+if(cookies_cucumber[`name`] === `cucumbers`){
+    cucumbers_display();
+}
+if(cookies_potatoes[`name`] === `potatoes`){
+    potatoes_display();
+}
+if(cookies_squashes[`name`] === `squashes`){
+    squashes_display();
+}
+if(cookies_tomatoes[`name`] === `tomatoes`){
+    tomatoes_display();
 }
