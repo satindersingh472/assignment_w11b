a8c60fafb88ac09df2b701da2df6b3af17dea957
satinder <satindersingh472@gmail.com>
2022-07-22 14:16:09 -0600

    working on

--- js/checkout.js
+++ js/checkout.js
@@ -2,13 +2,23 @@ let main = document.getElementById(`main`);
 
 let cookies_carrots_json = Cookies.get(`carrots`);
 let cookies_carrots = JSON.parse(cookies_carrots_json);
-if (cookies_carrots[`name`] === `carrots`) {
-    carrots_display();
-}
+main[`innerHTML`] +=
+    `<section>
+<h3> You have Selected ${cookies_carrots[`name`]} </h3>
+<p> Price ${cookies_carrots[`price`]}</p>
+</section>`;
 
 let cookies_cucumbers_json = Cookies.get(`cucumbers`);
 let cookies_cucumber = JSON.parse(cookies_cucumbers_json);
-
+main[`innerHTML`] +=
+    `<section>
+<h3>You have selected ${cookies_cucumber[`name`]}</h3>
+<p>Price ${cookies_cucumber[`price`]}</p>
+<div>
+<button id="remove_cucumber"> Delete </button>
+</div>
+</section>`
+    ;
 let cookies_potatoes_json = Cookies.get(`potatoes`);
 let cookies_potatoes = JSON.parse(cookies_potatoes_json);
 
@@ -22,24 +32,12 @@ let cookies_tomatoes = JSON.parse(cookies_tomatoes_json);
 
 // function carrots display will display description,name,price and image of the cookie selected
 function carrots_display(details) {
-    main[`innerHTML`] +=
-        `<section>
-<h3> You have Selected ${cookies_carrots[`name`]} </h3>
-<p> Price ${cookies_carrots[`price`]}</p>
-</section>`;
+
 }
 // cucumbers display function will display description of the item selected
 
 function cucumbers_display(details) {
-    main[`innerHTML`] +=
-        `<section>
-    <h3>You have selected ${cookies_cucumber[`name`]}</h3>
-    <p>Price ${cookies_cucumber[`price`]}</p>
-    <div>
-    <button id="remove_cucumber"> Delete </button>
-    </div>
-    </section>`
-        ;
+
 }
 function potatoes_display(details) {
     main[`innerHTML`] +=
@@ -66,7 +64,9 @@ function tomatoes_display(details) {
 
 
 //if statement will check if carrots cookies selected and has name value equal to carrots
-
+if (cookies_carrots[`name`] === `carrots`) {
+    carrots_display();
+}
 if (cookies_cucumber[`name`] === `cucumbers`) {
     cucumbers_display();
 }
